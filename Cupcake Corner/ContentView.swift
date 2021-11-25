3be7c5cd97c0b080705557d554b06d18ae137400
Pau Valverde Molina <pvalverdemolina@gmail.com>
2021-11-25 10:19:41 +0100

    Initial Commit

---
+++ Cupcake Corner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Cupcake Corner
+//
+//  Created by Pau Valverde Molina on 25/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Cupcake Corner/Cupcake_CornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cupcake_CornerApp.swift
+//  Cupcake Corner
+//
+//  Created by Pau Valverde Molina on 25/11/21.
+//
+
+import SwiftUI
+
+@main
+struct Cupcake_CornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
