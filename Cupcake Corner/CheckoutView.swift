493abf55b5cc011d83225373adf0eca686a898e0
Pau Valverde Molina <pvalverdemolina@gmail.com>
2021-11-26 16:28:06 +0100

    Added network request to send data of the order to reqres.in and show the order placed in an alert showing the data received back

--- Cupcake Corner/CheckoutView.swift
+++ Cupcake Corner/CheckoutView.swift
@@ -11,6 +11,9 @@ struct CheckoutView: View {
     
     @ObservedObject var order: Order
     
+    @State private var confirmationMessage = ""
+    @State private var showingConformation = false
+    
     var body: some View {
         ScrollView {
             VStack {
@@ -26,12 +29,44 @@ struct CheckoutView: View {
                 Text("You total order is \(order.cost, format: .currency(code: "USD"))")
                     .font(.title)
                 
-                Button("Place order", action: {})
-                    .padding()
+                Button("Place order") {
+                    Task {
+                        await placeOrder()
+                    }
+                }
+                .padding()
             }
         }
         .navigationTitle("Check out")
         .navigationBarTitleDisplayMode(.inline)
+        .alert("Thank you", isPresented: $showingConformation) {
+            Button("OK") { }
+        } message: {
+            Text(confirmationMessage)
+        }
+    }
+    
+    func placeOrder() async {
+        guard let encodedOrder = try? JSONEncoder().encode(order) else {
+            print("Failed to encode order")
+            return
+        }
+        
+        let url = URL(string: "https://reqres.in/api/cupcakes")!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        
+        do {
+            let (data, _) = try await URLSession.shared.upload(for: request, from: encodedOrder)
+            
+            let decodedOrder = try JSONDecoder().decode(Order.self, from: data)
+            confirmationMessage = "Your order: \(Order.types[decodedOrder.type].lowercased()) x \(decodedOrder.quantity) is on the way"
+            showingConformation = true
+            
+        } catch {
+            print("Failed checkout")
+        }
     }
 }
 
